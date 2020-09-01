725cad4d2a14f13c76276b9822d7d424b2124679
ombe1229 <h3236516@gmail.com>
2020-09-01 21:25:52 +0900

    SCP피흡, 피흡메세지 추가

--- EventHandlers.cs
+++ EventHandlers.cs
@@ -34,6 +34,41 @@ namespace DogeServerPlugin{
         }
 
 
+    internal void OnPlayerDeath(DiedEventArgs ev){
+        if (_pluginInstance.Config.ScpHealingEnable && ev.Killer.Team == Team.SCP && ev.Killer != ev.Target){
+                int heal = 0;
+                switch (ev.Killer.Role)
+                {
+                    case RoleType.Scp173 :
+                        heal = _pluginInstance.Config.Healing173;
+                        break;
+                    case RoleType.Scp096 :
+                        heal = _pluginInstance.Config.Healing096;
+                        break;
+                    case RoleType.Scp049 :
+                        heal = _pluginInstance.Config.Healing049;
+                        break;
+                    case RoleType.Scp0492 :
+                        heal = _pluginInstance.Config.Healing0492;
+                        break;
+                    case RoleType.Scp106 :
+                        heal = _pluginInstance.Config.Healing106;
+                        break;
+                    case RoleType.Scp93953 :
+                        heal = _pluginInstance.Config.Healing939;
+                        break;
+                    case RoleType.Scp93989 :
+                        heal = _pluginInstance.Config.Healing939;
+                        break;
+                }
+
+                if (ev.Killer.MaxHealth <= ev.Killer.Health + heal) ev.Killer.Health = ev.Killer.MaxHealth;
+                else ev.Killer.Health += heal;
+                ev.Killer.Broadcast(10,$"당신은 인간을 처치하여 체력 <color=red>${heal}</color>만큼 회복했습니다.\n현재 HP:<color=red>{ev.Killer.Health}</color>");
+            }
+    }
+
+
 
 
         private string FilteringNickname(Exiled.API.Features.Player player){
